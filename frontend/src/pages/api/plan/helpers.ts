e78df6a9cbd3b890bc37c865fde54513e0e53026
Sparsh Modi <sparshmodi2016@gmail.com>
2024-06-12 18:30:21 -0400

    commented old api ep

--- frontend/src/pages/api/plan/helpers.ts
+++ frontend/src/pages/api/plan/helpers.ts
@@ -1,123 +1,129 @@
 
-import { DateTime } from 'luxon'
-import { FinalClass, PlanClass, UwaterlooClass, UwaterlooSection } from "@/types"
+// import { DateTime } from 'luxon'
+// import { FinalClass, PlanClass, UwaterlooClass, UwaterlooSection } from "@/types"
 
-type Class = PlanClass
+// type Class = PlanClass
 
-export const modifyFetchedData = (classes: UwaterlooClass[]): PlanClass[] => {
-    const modifiedData: PlanClass[] = []
+// export const modifyFetchedData = (classes: UwaterlooClass[]): PlanClass[] => {
+//     const modifiedData: PlanClass[] = []
 
-    for (const uwClass of classes) {
-        const newClass: PlanClass = {}
-        for (const section of uwClass) {
-            if (newClass[section.courseComponent]) {
-                newClass[section.courseComponent].push(section)
-            } else {
-                newClass[section.courseComponent] = [section]
-            }
-        }
-        modifiedData.push(newClass)
-    }
+//     for (const uwClass of classes) {
+//         const newClass: PlanClass = {}
+//         for (const section of uwClass) {
+//             if (newClass[section.courseComponent]) {
+//                 newClass[section.courseComponent].push(section)
+//             } else {
+//                 newClass[section.courseComponent] = [section]
+//             }
+//         }
+//         modifiedData.push(newClass)
+//     }
 
-    return modifiedData
-}
+//     return modifiedData
+// }
 
-const sectionsOverlap = (section1: UwaterlooSection, section2: UwaterlooSection): boolean => {
-    if (section1.scheduleData === null || section2.scheduleData === null) return false
-    for (const scheduleData1 of section1.scheduleData) {
-        for (const scheduleData2 of section2.scheduleData) {
-            if (
-                scheduleData1.classMeetingDayPatternCode.includes(scheduleData2.classMeetingDayPatternCode) &&
-                DateTime.fromISO(scheduleData1.classMeetingStartTime) <= DateTime.fromISO(scheduleData2.classMeetingEndTime) &&
-                DateTime.fromISO(scheduleData1.classMeetingEndTime) >= DateTime.fromISO(scheduleData2.classMeetingStartTime)
-            ) {
-                return true
-            }
-        }
-    }
-    return false
-}
+// const sectionsOverlap = (section1: UwaterlooSection, section2: UwaterlooSection): boolean => {
+//     if (section1.scheduleData === null || section2.scheduleData === null) return false
+//     for (const scheduleData1 of section1.scheduleData) {
+//         for (const scheduleData2 of section2.scheduleData) {
+//             if (
+//                 scheduleData1.classMeetingDayPatternCode.includes(scheduleData2.classMeetingDayPatternCode) &&
+//                 DateTime.fromISO(scheduleData1.classMeetingStartTime) <= DateTime.fromISO(scheduleData2.classMeetingEndTime) &&
+//                 DateTime.fromISO(scheduleData1.classMeetingEndTime) >= DateTime.fromISO(scheduleData2.classMeetingStartTime)
+//             ) {
+//                 return true
+//             }
+//         }
+//     }
+//     return false
+// }
 
-export const generateCombinationsForClass = (inputClass: Class): FinalClass[] => {
-    const keys = Object.keys(inputClass)
+// export const generateCombinationsForClass = (inputClass: Class): FinalClass[] => {
+//     const keys = Object.keys(inputClass)
 
-    const generateAllCombinations = (index: number, currentCombination: FinalClass): FinalClass[] => {
-        if (index === keys.length) {
-            return [currentCombination]
-        }
+//     const generateAllCombinations = (index: number, currentCombination: FinalClass): FinalClass[] => {
+//         if (index === keys.length) {
+//             return [currentCombination]
+//         }
     
-        const key = keys[index]
-        const finalClassValues = inputClass[key]
-        const combinations: FinalClass[] = []
+//         const key = keys[index]
+//         const finalClassValues = inputClass[key]
+//         const combinations: FinalClass[] = []
     
-        for (const finalClass of finalClassValues) {
-            const newCombination = { ...currentCombination, [key]: finalClass }
-            combinations.push(...generateAllCombinations(index + 1, newCombination))
-        }
+//         for (const finalClass of finalClassValues) {
+//             const newCombination = { ...currentCombination, [key]: finalClass }
+//             combinations.push(...generateAllCombinations(index + 1, newCombination))
+//         }
     
-        return combinations
-    }
+//         return combinations
+//     }
 
-    const allCombinations = generateAllCombinations(0, {})
+//     const allCombinations = generateAllCombinations(0, {})
 
-    const doesNotOverlap = (classToCheck: FinalClass) => {
-        const sectionsArray = Object.values(classToCheck)
-        for (let i = 0; i < sectionsArray.length; i++) {
-            for (let j = i + 1; j < sectionsArray.length; j++) {
-                if (sectionsOverlap(sectionsArray[i], sectionsArray[j])) return false
-            }
-        }
-        return true
-    }
+//     const doesNotOverlap = (classToCheck: FinalClass) => {
+//         const sectionsArray = Object.values(classToCheck)
+//         for (let i = 0; i < sectionsArray.length; i++) {
+//             for (let j = i + 1; j < sectionsArray.length; j++) {
+//                 if (sectionsOverlap(sectionsArray[i], sectionsArray[j])) return false
+//             }
+//         }
+//         return true
+//     }
     
-    return allCombinations.filter(classToFilter => doesNotOverlap(classToFilter))
-}
+//     return allCombinations.filter(classToFilter => doesNotOverlap(classToFilter))
+// }
 
-export const generateSchedules = (arrays: FinalClass[][]): FinalClass[][] => {
+// export const generateSchedules = (arrays: FinalClass[][]): FinalClass[][] => {
 
-    const generateAllScheduleCombinations = (arrays: FinalClass[][]): FinalClass[][] => {
-        if (arrays.length === 0) {
-            return [[]]
-        }
+//     const generateAllScheduleCombinations = (arrays: FinalClass[][]): FinalClass[][] => {
+//         if (arrays.length === 0) {
+//             return [[]]
+//         }
         
-        const currentArray = arrays[0]
-        const remainingArrays = arrays.slice(1)
-        const combinations: FinalClass[][] = []
+//         const currentArray = arrays[0]
+//         const remainingArrays = arrays.slice(1)
+//         const combinations: FinalClass[][] = []
         
-        const lowerCombinations = generateSchedules(remainingArrays)
+//         const lowerCombinations = generateSchedules(remainingArrays)
         
-        for (const y of currentArray) {
-            for (const lowerCombination of lowerCombinations) {
-            combinations.push([y, ...lowerCombination])
-            }
-        }
+//         for (const y of currentArray) {
+//             for (const lowerCombination of lowerCombinations) {
+//             combinations.push([y, ...lowerCombination])
+//             }
+//         }
         
-        return combinations
-    }
+//         return combinations
+//     }
 
-    const classesOverlap = (class1: FinalClass, class2: FinalClass): boolean => {
-        const courseComponents1 = Object.keys(class1) 
-        const courseComponents2 = Object.keys(class2) 
+//     const classesOverlap = (class1: FinalClass, class2: FinalClass): boolean => {
+//         const courseComponents1 = Object.keys(class1) 
+//         const courseComponents2 = Object.keys(class2) 
 
-        for (const courseComponent1 of courseComponents1) {
-            for (const courseComponent2 of courseComponents2) {
-                if (sectionsOverlap(class1[courseComponent1], class2[courseComponent2])) return true
-            }
-        }
-        return false
-    }
+//         for (const courseComponent1 of courseComponents1) {
+//             for (const courseComponent2 of courseComponents2) {
+//                 if (sectionsOverlap(class1[courseComponent1], class2[courseComponent2])) return true
+//             }
+//         }
+//         return false
+//     }
 
-    const allCombinations = generateAllScheduleCombinations(arrays)
+//     const allCombinations = generateAllScheduleCombinations(arrays)
 
-    const doesNotOverlap = (scheduleToCheck: FinalClass[]) => {
-        for (let i = 0; i < scheduleToCheck.length; i++) {
-            for (let j = i + 1; j < scheduleToCheck.length; j++) {
-                if (classesOverlap(scheduleToCheck[i], scheduleToCheck[j])) return false
-            }
-        }
-        return true
-    }
+//     const doesNotOverlap = (scheduleToCheck: FinalClass[]) => {
+//         for (let i = 0; i < scheduleToCheck.length; i++) {
+//             for (let j = i + 1; j < scheduleToCheck.length; j++) {
+//                 if (classesOverlap(scheduleToCheck[i], scheduleToCheck[j])) return false
+//             }
+//         }
+//         return true
+//     }
     
-    return allCombinations.filter(scheduleToFilter => doesNotOverlap(scheduleToFilter))
-}
+//     return allCombinations.filter(scheduleToFilter => doesNotOverlap(scheduleToFilter))
+// }
 
+
+// export type UwaterlooClass = UwaterlooSection[]
+
+// export type PlanClass = {[courseComponent: string]: UwaterlooSection[]}
+
+// export type FinalClass = {[courseComponent: string]: UwaterlooSection}
--- frontend/src/pages/api/plan/index.ts
+++ frontend/src/pages/api/plan/index.ts
@@ -1,48 +1,48 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import axios from 'axios'
-import { UW_SCHEDULES_ENDPOINT } from '@/constants'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '../auth/[...nextauth]'
-import { FinalClass, UwaterlooClass, UwaterlooSection } from '@/types'
-import { generateCombinationsForClass, generateSchedules, modifyFetchedData } from './helpers'
+// import { NextApiRequest, NextApiResponse } from 'next'
+// import axios from 'axios'
+// import { UW_SCHEDULES_ENDPOINT } from '@/constants'
+// import { getServerSession } from 'next-auth'
+// import { authOptions } from '../auth/[...nextauth]'
+// import { FinalClass, UwaterlooClass, UwaterlooSection } from '@/types'
+// import { generateCombinationsForClass, generateSchedules, modifyFetchedData } from './helpers'
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const session = await getServerSession(req, res, authOptions)
+// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+//     const session = await getServerSession(req, res, authOptions)
 
-    if (session) {
-        const courses = req.query.course as string
-        const coursesArray: string[] = courses.split(',').map(str => str.trim())
+//     if (session) {
+//         const courses = req.query.course as string
+//         const coursesArray: string[] = courses.split(',').map(str => str.trim())
     
-        let coursesData: UwaterlooClass[] = []
+//         let coursesData: UwaterlooClass[] = []
 
-        await Promise.all(coursesArray.map(async (course: string) => {
-            try {
-                const response = await axios.get<UwaterlooClass>(UW_SCHEDULES_ENDPOINT + course, {
-                    headers: {
-                        'x-api-key': process.env.UWATERLOO_KEY
-                    }
-                })
-                const data = response.data as unknown as UwaterlooSection[]
-                coursesData.push(data)
-            } catch (err) {
-                console.error(err)
-            }
-        }))
+//         await Promise.all(coursesArray.map(async (course: string) => {
+//             try {
+//                 const response = await axios.get<UwaterlooClass>(UW_SCHEDULES_ENDPOINT + course, {
+//                     headers: {
+//                         'x-api-key': process.env.UWATERLOO_KEY
+//                     }
+//                 })
+//                 const data = response.data as unknown as UwaterlooSection[]
+//                 coursesData.push(data)
+//             } catch (err) {
+//                 console.error(err)
+//             }
+//         }))
 
-        const modifiedData = modifyFetchedData(coursesData)
+//         const modifiedData = modifyFetchedData(coursesData)
 
-        let allCombinationsOfClasses: FinalClass[][] = []
-        for (const inputClass of modifiedData) {
-            const returnedData = generateCombinationsForClass(inputClass)
-            allCombinationsOfClasses.push(returnedData)
-        }
+//         let allCombinationsOfClasses: FinalClass[][] = []
+//         for (const inputClass of modifiedData) {
+//             const returnedData = generateCombinationsForClass(inputClass)
+//             allCombinationsOfClasses.push(returnedData)
+//         }
 
-        const data = generateSchedules(allCombinationsOfClasses)
+//         const data = generateSchedules(allCombinationsOfClasses)
 
-        res.status(200).send(data)
-    } else {
-        res.status(401).end()
-    }
-}
+//         res.status(200).send(data)
+//     } else {
+//         res.status(401).end()
+//     }
+// }
 
-export default handler
+// export default handler
